e314603d8b373368ddb23aca7403a9fe3b3d5734
viktor-kirshin <kmbmetcom2005@bk.ru>
2024-10-03 15:58:59 +0400

    drop fix

--- My project/Assets/Scripts/Drop/dropSpawn.cs
+++ My project/Assets/Scripts/Drop/dropSpawn.cs
@@ -8,8 +8,9 @@ public class dropSpawn : MonoBehaviour
     [SerializeField] private GameObject dropPool;
     [HideInInspector] public bool isActiveSpawnDrop = false;
 
+    private Transform player;
     private float dropMaxCount = 20;
-    private float dropSpawndelay = 1f;
+    private float dropSpawndelay = 10;
     private const float border = 99.0f;
 
     void Start()
@@ -18,7 +19,7 @@ public class dropSpawn : MonoBehaviour
     }
     void Update()
     {
-        
+        player = GameObject.Find("Player").transform;
     }
 
     public IEnumerator spawnDrop()
@@ -31,7 +32,7 @@ public class dropSpawn : MonoBehaviour
 
             if (dropPool.transform.childCount < dropMaxCount)
             {
-                Vector2 spawnPos = new Vector2(Random.Range(-border, border + 1), Random.Range(-border, border + 1));
+                Vector2 spawnPos = new Vector2(Random.Range(player.position.x-30, player.position.x+30), Random.Range(player.position.y-30, player.position.y+30));
                 Instantiate(drop, spawnPos, Quaternion.identity, dropPool.transform);
             }
 
